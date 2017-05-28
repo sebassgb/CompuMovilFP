7689f4ec9ae265f28d018219723ab752733884f5
root <sebastian.gbotero@gmail.com>
2017-05-28 16:16:13 -0500

    Nuevos

--- js/example.js
+++ js/example.js
@@ -8,25 +8,66 @@ var config = {
 };
 
 firebase.initializeApp(config);  // objeto para aceder a la bd
-showTable();
 
 
-function showTable() {//Traemos datos desde firebase
-    var database = firebase.database(); // objeto para hacer uso de la bd
-    var referencia = 'productos/';
-    var bdEventos=database.ref(referencia);
 
-    bdEventos.on('value', function(datos) {//Traemos de la BD los productos
-      var i=0, j=0;
-      var datos =  datos.val();
-     $.each(datos,function(indice,valor)//Recorremos el arbol obteniendo los datos
-     {
-        document.querySelector("#category"+i).innerHTML =valor.categoria;
-        document.querySelector("#price"+i).innerHTML =valor.precio;
-        document.querySelector("#available"+i).innerHTML =valor.disponible;
-         i++;
-     });
-},function(objetoError){
-       alert("error en la lectura "+objetoError.code);
-    });
+function putInfo(nombreProducto){//Obtenemos el seleccionado por el usuario
+  document.querySelector("#nomProduct").value = nombreProducto;
+}
+
+function makeOrder(){
+  if(verificaDatos()){
+  var database = firebase.database();
+  var nombreProduct=document.querySelector("#nomProduct").value;
+  var direccion=document.querySelector("#dirProduct").value;
+  var tel=document.querySelector("#telfProduct").value;
+  var dia=document.querySelector("#diaProduct").value;
+  var correou=document.querySelector("#correoProduct").value;
+  var numcard=document.querySelector("#numtProduct").value;
+			var referencia="ordenes/";
+
+			var ordenCreada={
+        producto: nombreProduct,
+        dir: direccion,
+        num_card: numcard,
+        pref: dia,
+        telf: tel,
+        correo: correou
+			   }
+			database.ref(referencia).push(ordenCreada);
+      setTimeout(redirect, 2000);
+      }
+    }
+
+    function redirect() {
+    alert('Orden Generada con éxito');
+    location.href="lobby.html";
+  }
+
+function verificaDatos(){//Verificamos que todo este correcto
+  if(document.querySelector("#nomProduct").value.length==0){
+    alert("Seleccione un producto de la lista");
+    return false;
+  }
+   if (document.querySelector("#dirProduct").value.length==0) {
+    alert("falta dirección");
+    return false;
+   }
+   if (document.querySelector("#telfProduct").value.length==0) {
+    alert("falta el Teléfono");
+    return false;
+   }
+   if (document.querySelector("#diaProduct").value.length==0) {
+    alert("falta el dia preferido");
+    return false;
+   }
+   if (document.querySelector("#correoProduct").value.length==0) {
+    alert("falta el correo");
+    return false;
+   }
+   if (document.querySelector("#numtProduct").value.length==0) {
+    alert("falta el Número de tarjeta");
+    return false;
+   }
+return true;
 }
